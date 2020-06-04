9c7c35a77ea1cc36d300bc21588defeece846f5d
Sidney Lower <s.lower@c34b-s35.ufhpc>
2020-06-04 14:42:36 -0400

    adding dust screen model param file

---
+++ experiments/dust_screen/tau/run_prosp.py
@@ -0,0 +1,352 @@
+import numpy as np
+from sedpy.observate import load_filters
+from prospect.fitting import fit_model
+from prospect.io import write_results as writer
+from prospect.models import priors, transforms, sedmodel
+from scipy.stats import truncnorm
+import os, sys
+
+
+run_params = {'verbose':False,
+              'debug':False,
+              'output_pickles': False,
+              # dynesty Fitter parameters
+              'nested_bound': 'multi', # bounding method
+              'nested_sample': 'auto', # sampling method
+              'nested_nlive_init': 400,
+              'nested_nlive_batch': 200,
+              'nested_bootstrap': 0,
+              'nested_dlogz_init': 0.01,
+              'nested_weight_kwargs': {"pfrac": 1.0},
+              }
+
+def find_nearest(array,value):
+    idx = (np.abs(np.array(array)-value)).argmin()
+    return idx
+
+
+def build_sps(zcontinuous=1, compute_vega_mags=False, **extras):
+    from prospect.sources import CSPSpecBasis
+    sps = CSPSpecBasis(zcontinuous=zcontinuous,
+                       compute_vega_mags=compute_vega_mags)
+
+    return sps
+
+def build_noise(**extras):
+    return None, None
+
+
+
+
+class MassMet(priors.Prior):
+    """A Gaussian prior designed to approximate the Gallazzi et al. 2005 
+    stellar mass--stellar metallicity relationship.
+    """
+
+    prior_params = ['mass_mini', 'mass_maxi', 'z_mini', 'z_maxi']
+    distribution = truncnorm
+    massmet = np.loadtxt('/ufrc/narayanan/s.lower/simSEDs/simbam25n512_newfof/gallazzi_05_massmet.txt')
+
+    def __len__(self):
+        return 2
+
+    def scale(self,mass):
+        upper_84 = np.interp(mass, self.massmet[:,0], self.massmet[:,3]) 
+        lower_16 = np.interp(mass, self.massmet[:,0], self.massmet[:,2])
+        return (upper_84-lower_16)
+
+    def loc(self,mass):
+        return np.interp(mass, self.massmet[:,0], self.massmet[:,1])
+
+    def get_args(self,mass):
+        a = (self.params['z_mini'] - self.loc(mass)) / self.scale(mass)
+        b = (self.params['z_maxi'] - self.loc(mass)) / self.scale(mass)
+        return [a, b]
+
+    @property
+    def range(self):
+        return ((self.params['mass_mini'], self.params['mass_maxi']),\
+                (self.params['z_mini'], self.params['z_maxi']))
+
+    def bounds(self, **kwargs):
+        if len(kwargs) > 0:
+            self.update(**kwargs)
+        return self.range
+
+    def __call__(self, x, **kwargs):
+        if len(kwargs) > 0:
+            self.update(**kwargs)
+        p = np.atleast_2d(np.zeros_like(x))
+        a, b = self.get_args(x[...,0])
+        p[...,1] = self.distribution.pdf(x[...,1], a, b, loc=self.loc(x[...,0]), scale=self.scale(x[...,0]))
+        with np.errstate(invalid='ignore'):
+            p[...,1] = np.log(p[...,1])
+        return p
+
+    def sample(self, nsample=None, **kwargs):
+        if len(kwargs) > 0:
+            self.update(**kwargs)
+        mass = np.random.uniform(low=self.params['mass_mini'],high=self.params['mass_maxi'],size=nsample)
+        a, b = self.get_args(mass)
+        met = self.distribution.rvs(a, b, loc=self.loc(mass), scale=self.scale(mass), size=nsample)
+
+        return np.array([mass, met])
+
+    def unit_transform(self, x, **kwargs):
+        if len(kwargs) > 0:
+            self.update(**kwargs)
+        mass = x[0]*(self.params['mass_maxi'] - self.params['mass_mini']) + self.params['mass_mini']
+        a, b = self.get_args(mass)
+        met = self.distribution.ppf(x[1], a, b, loc=self.loc(mass), scale=self.scale(mass))
+        return np.array([mass,met])
+
+
+def massmet_to_logmass(massmet=None,**extras):
+    return massmet[0]
+
+def massmet_to_logzsol(massmet=None,**extras):
+    return massmet[1]
+
+
+def log_masses_to_mass(logmass=None, **extras):
+    return 10**logmass
+
+
+
+
+model_params = []
+
+model_params.append({'name': "lumdist", "N": 1, "isfree": False,"init": 1.0e-5,
+"units": "Mpc"})
+
+
+model_params.append({'name': 'pmetals', 'N': 1,
+                        'isfree': False,
+                        'init': -99,
+                        'units': '', 'prior': None})
+model_params.append({'name': 'massmet', 'N': 2,
+                        'isfree': True,
+                        'init': np.array([10,-0.5]), 'prior': None})
+
+model_params.append({'name': 'logmass', 'N': 1,
+                        'isfree': False,
+                        'depends_on': massmet_to_logmass,
+                        'init': 10.0,
+                        'units': 'Msun', 'prior': None})
+
+
+model_params.append({'name': 'logzsol', 'N': 1,
+                        'isfree': False,
+                        'init': -0.5,
+                        'depends_on': massmet_to_logzsol,
+                        'units': r'$\log (Z/Z_\odot)$', 'prior': None})
+
+model_params.append({'name': "sfh", "N": 1, "isfree": False, "init": 4, "units": "FSPS index"})
+
+
+model_params.append({'name': "mass", 'N': 3, 'isfree': False, 'init': 1., 'units': r'M$_\odot$',
+                          'depends_on':log_masses_to_mass})
+
+model_params.append({'name': 'tau', 'N': 1,
+                             'isfree': True,
+                             'init': 1,
+                     'prior': priors.LogUniform(mini=0.1, maxi=30)})
+
+model_params.append({'name': 'tage', 'N': 1,
+                             'isfree': True,
+                     'init': 5,
+                     'prior': priors.TopHat(mini=0.1, maxi=14.1)})
+
+
+
+model_params.append({'name': 'imf_type', 'N': 1,
+                             'isfree': False,
+                             'init': 2, #Kroupa                                                    
+                     'prior': ' ' })
+
+model_params.append({'name': 'dust_type', 'N': 1,
+                        'isfree': False,
+                        'init': 0,
+                        'units': 'index', 'prior': None})
+
+model_params.append({'name': 'dust1', 'N': 1,
+                        'isfree': False,
+                        'init': 0.7,
+                        'units': '', 'prior': None})
+
+model_params.append({'name': 'dust2', 'N': 1,
+                        'isfree': False,
+                        'init': 0.7,
+                        'init_disp': 0.25,
+                        'disp_floor': 0.15,
+                        'units': '',
+                        'prior': priors.ClippedNormal(mini=0.0, maxi=2.0, mean=0.3, sigma=1)})
+
+model_params.append({'name': 'dust_index', 'N': 1,
+                        'isfree': False,
+                        'init': -0.7,
+                        'init_disp': 0.25,
+                        'disp_floor': 0.15,
+                        'units': '',
+                        'prior': priors.TopHat(mini=-1.8, maxi=0.4)})
+
+model_params.append({'name': 'dust1_index', 'N': 1,
+                        'isfree': False,
+                        'init': -1.0,
+                        'units': '', 'prior': None})
+
+
+model_params.append({'name': 'dust_tesc', 'N': 1,
+                        'isfree': False,
+                        'init': 7.0,
+                        'units': 'log(Gyr)', 'prior': None})
+
+model_params.append({'name': 'add_agb_dust_model', 'N': 1,
+                        'isfree': False,
+                        'init': 0,
+                        'units': None, 'prior': None})
+
+
+model_params.append({'name': 'add_dust_emission', 'N': 1,
+                        'isfree': False,
+                        'init': 1,
+                        'units': None, 'prior': None})
+
+model_params.append({'name': 'duste_gamma', 'N': 1,
+                        'isfree': True,
+                        'init': 0.01,
+                        'init_disp': 0.2,
+                        'disp_floor': 0.15,
+                        'units': None,
+                        'prior': priors.TopHat(mini=0.0, maxi=1.0)})
+
+model_params.append({'name': 'duste_umin', 'N': 1,
+                        'isfree': True,
+                        'init': 1.0,
+                        'init_disp': 5.0,
+                        'disp_floor': 4.5,
+                        'units': None,
+                        'prior': priors.TopHat(mini=0.1, maxi=30.0)})
+
+model_params.append({'name': 'duste_qpah', 'N': 1,
+                        'isfree': False,
+                        'init': 6,
+                        'init_disp': 3.0,
+                        'disp_floor': 3.0,
+                        'units': 'percent',
+                        'prior': priors.TopHat(mini=0.0, maxi=3.0)})
+
+
+
+####### Units ##########                                                                                          
+model_params.append({'name': 'peraa', 'N': 1,
+                     'isfree': False,
+                     'init': False, 'prior': None})
+
+model_params.append({'name': 'mass_units', 'N': 1,
+                     'isfree': False,
+                     'init': 'mformed', 'prior': None})
+
+
+
+
+
+def build_model(**kwargs):
+
+    n = [p['name'] for p in model_params]
+    model_params[n.index('massmet')]['prior'] = MassMet(z_mini=-1.6, z_maxi=0.19, mass_mini=7.5, mass_maxi=13)
+    # Now instantiate the model using this new dictionary of parameter specifications
+    model = sedmodel.SedModel(model_params)
+    
+
+    return model
+
+
+
+galex = ['galex_FUV', 'galex_NUV']
+hst_wfc3_uv  = ['wfc3_uvis_f275w', 'wfc3_uvis_f336w', 'wfc3_uvis_f475w','wfc3_uvis_f555w', 'wfc3_uvis_f606w', 'wfc3_uvis_f814w']
+sdss = ['sdss_u0', 'sdss_g0', 'sdss_r0', 'sdss_i0', 'sdss_z0']
+hst_wfc3_ir = ['wfc3_ir_f105w', 'wfc3_ir_f110w', 'wfc3_ir_f125w', 'wfc3_ir_f140w', 'wfc3_ir_f160w']
+spitzer_mips = ['spitzer_mips_24']
+herschel_pacs = ['herschel_pacs_70', 'herschel_pacs_100', 'herschel_pacs_160']
+herschel_spire = ['herschel_spire_250', 'herschel_spire_350', 'herschel_spire_500']
+filternames = (galex + hst_wfc3_uv + sdss + hst_wfc3_ir +  spitzer_mips + herschel_pacs + herschel_spire)
+
+
+def build_obs(**kwargs):
+
+    from hyperion.model import ModelOutput
+    from astropy import units as u
+    from astropy import constants
+    print('galaxy: ',sys.argv[1])
+    m = ModelOutput("/ufrc/narayanan/s.lower/pd_runs/simba_m25n512/snap305_dustscreen/snap305/snap305.galaxy"+str(sys.argv[1])+".rtout.sed")
+    wav,flux = m.get_sed(inclination=0,aperture=-1)
+    wav  = np.asarray(wav)*u.micron #wav is in micron                                                                                                        
+    wav = wav.to(u.AA)
+    flux = np.asarray(flux)*u.erg/u.s
+    dl = (10. * u.pc).to(u.cm)
+    flux /= (4.*3.14*dl**2.)
+    nu = constants.c.cgs/(wav.to(u.cm))
+    nu = nu.to(u.Hz)
+    flux /= nu
+    flux = flux.to(u.Jy)
+    maggies = flux / 3631.
+
+    filters_unsorted = load_filters(filternames)
+    waves_unsorted = [x.wave_mean for x in filters_unsorted]
+    filters = [x for _,x in sorted(zip(waves_unsorted,filters_unsorted))]
+    flx = []
+    flxe = []
+
+    for i in range(len(filters)):
+        flux_range = []
+        wav_range = []
+        for j in filters[i].wavelength:
+            flux_range.append(maggies[find_nearest(wav.value,j)].value)
+            wav_range.append(wav[find_nearest(wav.value,j)].value)
+        a = np.trapz(wav_range * filters[i].transmission* flux_range, wav_range, axis=-1)
+        b = np.trapz(wav_range * filters[i].transmission, wav_range)
+        flx.append(a/b)
+        flxe.append(0.03* flx[i])
+
+    flx = np.asarray(flx)
+    flxe = np.asarray(flxe)
+    flux_mag = flx
+    unc_mag = flxe
+
+    obs = {}
+    obs['filters'] = filters
+    obs['maggies'] = flux_mag
+    obs['maggies_unc'] = unc_mag
+    obs['phot_mask'] = np.isfinite(flux_mag)
+    obs['wavelength'] = None
+    obs['spectrum'] = None
+
+    return obs
+
+
+def build_all(**kwargs):
+
+    return (build_obs(**kwargs), build_model(**kwargs),
+            build_sps(**kwargs), build_noise(**kwargs))
+
+
+
+if __name__ == '__main__':
+
+    obs, model, sps, noise = build_all(**run_params)
+    run_params["sps_libraries"] = sps.ssp.libraries
+    hfile = "/ufrc/narayanan/s.lower/simSEDs/simbam25n512_newfof/experiments/dust_screen/tau/galaxy"+sys.argv[1]+".h5"
+
+    print('Running fits')
+    output = fit_model(obs, model, sps, noise, **run_params)
+    print('Done. Writing now')
+    writer.write_hdf5(hfile, run_params, model, obs,
+                  output["sampling"][0], output["optimization"][0],
+                  tsample=output["sampling"][1],
+                  toptimize=output["optimization"][1])
+
+    try:
+        hfile.close()
+    except(AttributeError):
+        pass
